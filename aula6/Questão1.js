6ac3f264296b40d6c6e008b51efdffe12286d229
Ramon Lima Rocha <ramon.lrocha@dinfor.uniceub>
2024-09-09 18:55:39 -0300

    vain

---
+++ aula6/Questão1.js
@@ -0,0 +1,72 @@
+//*1*\\
+
+//var a = 1
+//var b = 2
+//console.log(a+b);
+
+//*2*\\
+
+//var idade1 = 67
+//var idade2 = 4
+//console.log(idade1-idade2);
+
+//*3*\\
+
+//var preçouni = 52
+//var quantidade = 10
+//console.log(preçouni*quantidade);
+
+//*4*\\
+
+//var numerador = 56
+//var denominador = 5
+//console.log(numerador/denominador);
+//console.log(numerador%denominador)
+
+//*5*\\
+
+//var altura = 4
+//var largura = 20
+//console.log(altura*largura);
+
+//*6*\\
+
+//var c = 5
+//console.log(c*9/5+32)
+
+//*7*\\
+
+//var nota1 = 9
+//var nota2 = 6.5
+//var nota3 = 3
+//const Media = nota1+nota2+nota3
+//console.log(Math.floor(Media))
+
+//*8*\\
+
+//var num1 = 7
+//var num2 = 99
+
+//if(num1 > num2){
+//    console.log(num1);
+//}else{
+//    console.log(num2);
+//}
+
+//*9*\\
+
+//var num1 = 4
+//var num2 = 3
+//var soma = '+'
+//var subtracao = '-'
+//console.log(soma = num1+num2);
+//console.log(subtracao = num1-num2);
+
+//*10*\\
+
+var min = 600
+
+const hour = Math.floor(min/60)
+const minu = min%60
+
+console.log(hour, "hr",minu,"min");
