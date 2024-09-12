e79e6d6d32b0dcc1e5c783448ffff20d9a75aef7
Ramon Lima Rocha <ramon.lrocha@dinfor.uniceub>
2024-09-12 18:54:08 -0300

    fds casa

---
+++ aula7/vain.js
@@ -0,0 +1,47 @@
+
+
+//function tabuada() {
+//  let m = 2
+//  for (let i = 0; i <= 10; i++) {
+//     let mult = i * m
+//     console.log(`${m} x ${i} = ${mult}`);
+// }
+
+
+
+//}
+//tabuada()
+
+
+
+//for (let i = 5; ; i-1) {
+
+  //  console.log(i);
+
+//}
+
+//for(let i = 1; i <=100; i++){
+  //  let soma = i
+
+   // console.log(i+soma)
+//}
+
+//for(let i = 0; i <=25; i++){
+
+    //console.log(i+i);
+//}
+
+function calculadora(num1, num2, ope){
+ 
+    if(ope == ("+")){
+        return num1 + num2
+        
+    }else if(ope=="-"){
+    return num1 - num2
+
+    }
+    
+}
+
+console.log(calculadora(7,7,"-"))
+
