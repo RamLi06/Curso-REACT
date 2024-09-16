4b3daaaf9fbe92bb9448bce11798265b12786644
Ramon Lima Rocha <ramon.lrocha@dinfor.uniceub>
2024-09-16 19:00:14 -0300

    boom

---
+++ Aula8/fodase.js
@@ -0,0 +1,50 @@
+//const vain = [45,4,5];
+
+//let sum = 0;
+
+//for(let i = 0; i< vain.length; i++){
+//   sum += vain[i]
+//}
+//console.log(sum);
+
+/*const array = [45, 99, 56, 1];
+
+let numero = 0
+
+for(let i = 0 ; i< array.length; i++){
+
+    if( array[i] > numero){
+
+        numero = array[i];
+    }
+    
+}
+console.log(numero);
+*/
+
+/*const array = [33, 4, 5, 6, 33, 2, 33];
+
+let numerogay = 4
+
+let h = 0
+
+for (let i = 0; i < array.length; i++) {
+
+    if(array[i] == numerogay){
+        h++
+    }
+
+
+}
+console.log(h);
+*/
+
+
+const vain = [45,4,5];
+
+let sum = vain;
+
+for(let i = 0; i< vain.length; i++){
+   sum *= vain[i]
+}
+console.log(sum);
