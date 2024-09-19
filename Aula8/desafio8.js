6cff39cc53708daf295d52ed84688b5ef9a13b8e
RamLi06 <ramon.limarocha.809@gmail.com>
2024-09-19 10:44:02 -0300

    vain

---
+++ Aula8/desafio8.js
@@ -0,0 +1,14 @@
+function Cmedia() {
+
+    let soma = notas.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+
+    let media = soma/notas.length;
+
+    return media;
+}
+
+const notas = [4.5, 6.3, 10, 5];
+
+let media = Cmedia(notas);
+
+console.log("a média de notas é: "+media)
