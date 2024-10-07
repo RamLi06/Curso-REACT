54c8e6fbf5c11a2eb8dbe663e6aa8132434b12f5
Ramon Lima Rocha <ramon.lrocha@dinfor.uniceub>
2024-10-07 18:45:59 -0300

    Segunda

---
+++ teste2/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Componets/Header"
+
+function App(){
+
+  return(
+  
+  <>
+  <Header></Header>
+  <h1>15 cavalos diferentes</h1>
+  
+  
+  </>
+  
+  )
+
+
+}
+
+export default App
---
+++ teste2/src/Componets/Header/index.jsx
@@ -0,0 +1,20 @@
+export default function Header(){
+
+
+    return(
+    
+    <>
+
+    <header>
+    <h1>Logo</h1>
+
+    <div id='header.list'>
+
+        <a href="">Contatos</a><br />
+        <a href="">Projetos</a>
+
+    </div>
+    
+    </header>
+    </>)
+}
